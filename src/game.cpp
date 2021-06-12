8e469b56348317a875bf11a913e0a44654fa2a32
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-06-12 15:28:13 +0200

    Made an OOP version of the program

--- src/game.cpp
+++ src/game.cpp
@@ -1,26 +1,100 @@
 #include "game.hpp"
 
-Game::Game()
+Game::Game() : m_player(1), m_gameBegan(false), m_darkMode(false), m_bgColor(WHITE), m_text("")
 {
+    for (size_t i = 0; i < m_grid.size(); i++)
+    {
+        for (size_t j = 0; j < m_grid[i].size(); j++)
+        {
+            m_grid[i][j].width = c_boxWidth;
+            m_grid[i][j].height = c_boxHeight;
 
+            m_grid[i][j].x = (c_boxWidth + c_lineThickness) * i;
+            m_grid[i][j].y = (c_screenHeight - c_screenWidth) + (c_boxHeight + c_lineThickness) * j;
+        }
+    }
+
+    for (size_t i = 0; i < m_board.size(); i++)
+        for (size_t j = 0; j < m_board[i].size(); j++)
+            m_board[i][j] = c_none;
+    
+    InitWindow(c_screenWidth, c_screenHeight, "Tic Tac Toe");
+
+    m_imgIcon = LoadImage("../assets/cross.png");
+    SetWindowIcon(m_imgIcon);
+    UnloadImage(m_imgIcon);
+
+    m_texBoard = LoadTexture("../assets/board.png");
+    m_texCross = LoadTexture("../assets/cross.png");
+    m_texCircle = LoadTexture("../assets/circle.png");
+
+    SetTargetFPS(c_fps);
 }
 
 Game::~Game()
 {
+    UnloadTexture(m_texCircle);
+    UnloadTexture(m_texCross);
+    UnloadTexture(m_texBoard);
 
+    CloseWindow();
 }
 
 void Game::Run()
 {
+    while (!WindowShouldClose() && !CheckVictory<c_side>(m_board, c_states, m_player) && !CheckDraw<c_side>(m_board, c_states, m_player))
+    {
+        this->Update();
+        this->Draw();
+    }
 
+    if (CheckVictory<c_side>(m_board, c_states, m_player))
+    {
+        m_text = "Player " + std::to_string(m_player) + " has won!";
+        DrawGameOverScreen<c_side>(c_fps, m_text, m_bgColor, m_board, m_grid, c_states, m_texBoard, m_texCross, m_texCircle);
+    }
+    else if (CheckDraw<c_side>(m_board, c_states, m_player))
+        DrawGameOverScreen<c_side>(c_fps, "Draw!", m_bgColor, m_board, m_grid, c_states, m_texBoard, m_texCross, m_texCircle);
 }
 
 void Game::Update()
 {
+    if (IsKeyPressed(KEY_F12)) TakeScreenshot("scr.png");
+    if (IsKeyPressed(KEY_ENTER)) m_darkMode = !m_darkMode;
 
+    if (!m_darkMode && m_bgColor != WHITE) m_bgColor = WHITE;
+    else if (m_darkMode && m_bgColor != BLACK) m_bgColor = BLACK;
+
+    for (size_t i = 0; i < m_grid.size(); i++)
+    {
+        for (size_t j = 0; j < m_grid[i].size(); j++)
+        {
+            if ((GetMouseX() >= m_grid[i][j].x) && (GetMouseX() <= m_grid[i][j].x + m_grid[i][j].width) && 
+                (GetMouseY() >= m_grid[i][j].y) && (GetMouseY() <= m_grid[i][j].y + m_grid[i][j].height) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                if (!m_gameBegan) m_gameBegan = true;
+                if (m_board[i][j] == c_none)
+                {
+                    m_board[i][j] = c_states[m_player];
+                    if (!CheckVictory<c_side>(m_board, c_states, m_player))
+                        m_player = (m_player == 1) ? 2 : 1;
+                    m_text = "Player " + std::to_string(m_player) + "'s turn!";
+                }
+                else m_text = "Something was already placed in that box!";
+            }
+        }
+    }
 }
 
 void Game::Draw()
 {
-    
+    BeginDrawing();
+
+        ClearBackground(m_bgColor);
+        DrawBoard<c_side>(m_board, m_grid, c_states, m_texBoard, m_texCross, m_texCircle);
+
+        if (m_gameBegan) DrawUpperText(m_text, ((m_text.length() > 16) ? 25 : 40), (m_darkMode ? WHITE : BLACK));
+        else DrawUpperText("Player 1 plays X, player 2 plays O", 30, (m_darkMode ? WHITE : BLACK));
+
+    EndDrawing();
 }
--- src/game.hpp
+++ src/game.hpp
@@ -13,6 +13,34 @@ public:
     void Run();
     void Update();
     void Draw();
+
+private:
+    // Constants
+    static const int c_side = 3;
+
+    const int c_screenWidth = 600;
+    const int c_screenHeight = 700;
+    const int c_fps = 60;
+    
+    const int c_lineThickness = 5;
+
+    const int c_boxWidth = (c_screenWidth - c_lineThickness) / c_side;
+    const int c_boxHeight = (c_screenWidth - c_lineThickness) / c_side;
+
+    const char c_none = ' ', c_cross = 'X', c_circle = 'O';
+    const std::array<char, c_side> c_states = {c_none, c_cross, c_circle};
+
+    // Attributes
+    int m_player;
+    bool m_gameBegan, m_darkMode;
+    Color m_bgColor;
+    std::string m_text;
+
+    std::array<std::array<Rectangle, c_side>, c_side> m_grid;
+    std::array<std::array<char, c_side>, c_side> m_board;
+
+    Image m_imgIcon;
+    Texture2D m_texBoard, m_texCross, m_texCircle;
 };
 
 #endif
